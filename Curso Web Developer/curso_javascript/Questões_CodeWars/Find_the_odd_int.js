eeef19393eed43000a852ca3353112e36dc9254c
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-07-07 21:46:23 -0300

    Update Find_the_odd_int.js

--- Curso Web Developer/curso_javascript/Questões_CodeWars/Find_the_odd_int.js
+++ Curso Web Developer/curso_javascript/Questões_CodeWars/Find_the_odd_int.js
@@ -1,4 +1,11 @@
 function findOdd(array) {
+    var string = ''
+    var control = ''
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      
+      
+    }
     return 0
   }
 
