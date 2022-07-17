e0a66113eb5cee51da90b394ce8b600bcf44c371
BrunoRRamos <brbosshd2@gmail.com>
2022-07-17 10:46:42 -0300

    questao

---
+++ Curso Web Developer/curso_javascript/Aulas/Arrays/student_ search.js
@@ -0,0 +1,14 @@
+const students = ['Juan', 'Pedro', 'Maria', 'Luis', 'Jorge'];
+const notes = [10, 9, 8, 7, 6];
+
+const search = (name) => {
+    if (students.includes(name) === true) {
+        let Index = students.indexOf(name)
+        return `Hello ${name}, your note is ${notes[Index]}.`
+    }else {
+        return `Student ${name} not found`
+    }
+    
+}
+
+console.log(search('Juan'))
