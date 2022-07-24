e377266bef24386b3ebdf1219487bb7716899353
BrunoRRamos <brbosshd2@gmail.com>
2022-07-24 15:04:59 -0300

    Object Orientation

--- Curso Web Developer/curso_javascript/Aulas/Objects/Object_Orientation.js
+++ Curso Web Developer/curso_javascript/Aulas/Objects/Object_Orientation.js
@@ -1,32 +1,37 @@
 //Creating a Class
-class student {
+class Student {
     constructor (name, age, school, course) {
         this.name = name
         this.age = age
         this.school = school
         this.course = course
-        
+        }
+
         //A Class Method
-        this.changeAge = function change(newAge)  {
+        changeAge(newAge) {
             this.age = newAge
         }
-    }
-}
 
-const bruno = new student('Bruno', 19, 'UFCG', 'Computer Science')
+        returnCourse() {
+            return this.course
+        }
+    }
+
+const bruno = new Student('Bruno', 19, 'UFCG', 'Computer Science')
 console.log(bruno)
 
 //Using a Class Method
 bruno.changeAge(20)
+console.log(bruno.returnCourse())
 console.log(bruno)
 
 //class Inheritance
-class programer extends student {
+class Programer extends Student {
     constructor(name, age, school, course, language) {
         super(name, age, school, course)
         this.language = language
     } 
 }
 
-const brunoProg = new programer('Bruno', 19, 'UFCG', 'Computer Science', 'JavaScript')
+const brunoProg = new Programer('Bruno', 19, 'UFCG', 'Computer Science', 'JavaScript')
 console.log(brunoProg)
