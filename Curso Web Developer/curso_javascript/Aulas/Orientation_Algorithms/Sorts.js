4f8caa865896af8499b48fa8406fa6bbd5fa853b
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-08-12 10:39:39 -0300

    .

--- Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Sorts.js
+++ Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Sorts.js
@@ -1,12 +1,11 @@
-arr = [25, 100, 15, 33, 50, 45, 20]
-
+const {edFolha, edData} = require('./Variables')
 
 //Bubble Sort
 function bubbleSort(array) {
     for (let i = 0; i < array.length; i++) {
         for (let j = 0; j < array.length; j++) {
             
-            if (array[i] < array[j]) {
+            if (array[i].price < array[j].price) {
                 let aux = array[j]
                 array[j] = array[i]
                 array[i] = aux
@@ -19,15 +18,27 @@ function bubbleSort(array) {
 function insertionSort(array) {
     for (let index = 1; index < array.length; index++) {
         let actual = index
+        let prev = index - 1
 
-        while (array[actual] < array[actual - 1]) {
+        while (array[actual].price < array[prev].price) {
             let aux = array[actual]
-            array[actual] = array[actual - 1]
-            array[actual - 1] = aux
+            array[actual] = array[prev]
+            array[prev] = aux
             actual--
         }
     }
 }
 
-insertionSort(arr)
-console.log(arr)
+// Get together Lists (Sorted)
+function getLists(array1, array2) {
+    let finalArray = [];
+    let posiActualArr1 = 0;
+    let posiActualArr2 = 0;
+
+    while (posiActualArr1 < array1.length && posiActualArr2 < array2.length) {
+        let actualarr1 = array1[posiActualArr1]
+        let actualarr2 = array2[posiActualArr2]
+    }
+
+    return finalArray
+}
---
+++ Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Variables.js
@@ -0,0 +1,43 @@
+const edFolha = [
+    {
+        name: 'JavaScript',
+        price: 20
+    },
+    {
+        name: 'Python',
+        price: 15
+    },
+    {
+        name: 'Typescript',
+        price: 30
+    },
+    {
+        name: 'Sass',
+        price: 10
+    },
+    {
+        name: 'PostGree',
+        price: 60
+    }
+]
+
+const edData = [
+    {
+        name: 'MongoDB',
+        price: 32
+    },
+    {
+        name: 'GraphQL',
+        price: 78
+    },
+    {
+        name: 'Java',
+        price: 24
+    },
+    {
+        name: 'Haskell',
+        price: 41
+    }
+]
+
+module.exports = {edFolha, edData}
