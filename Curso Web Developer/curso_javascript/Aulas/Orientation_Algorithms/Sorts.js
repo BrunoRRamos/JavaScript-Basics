e94f2b34c90d3ece6c2f76b47e3fa120c2b2cf8e
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-08-15 12:37:53 -0300

    .

--- Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Sorts.js
+++ Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Sorts.js
@@ -1,4 +1,4 @@
-const {edFolha, edData} = require('./Variables')
+const {edFolha, edData, extremeArray} = require('./Variables')
 
 //Bubble Sort
 function bubbleSort(array) {
@@ -29,16 +29,72 @@ function insertionSort(array) {
     }
 }
 
-// Get together Lists (Sorted)
+// Get together Arrays (Sorted);
+//(arr1 - sorted) and (arr2 - sorted);
 function getLists(array1, array2) {
     let finalArray = [];
     let posiActualArr1 = 0;
     let posiActualArr2 = 0;
+    let actual = 0;
 
     while (posiActualArr1 < array1.length && posiActualArr2 < array2.length) {
         let actualarr1 = array1[posiActualArr1]
         let actualarr2 = array2[posiActualArr2]
-    }
 
+        if (actualarr1.price < actualarr2.price) {
+            finalArray[actual] = actualarr1;
+            posiActualArr1++;
+            
+        } else {
+            finalArray[actual] = actualarr2;
+            posiActualArr2++;
+        }
+        actual++;
+
+        while (actualarr1 < array1.length) {
+            finalArray[actual] = actualarr1;
+            posiActualArr1++;
+            actual++
+        }
+
+        while (actualarr2 < array2.length) {
+            finalArray[actual] = actualarr2;
+            posiActualArr2++;
+            actual++
+        }
+    }
     return finalArray
 }
+
+// Merge Sort
+function ord(part1, part2) {
+    let actualPosiPart1 = 0;
+    let actualPosiPart2 = 0;
+    const result  = []
+
+    while (actualPosiPart1 < part1.length && actualPosiPart2 < part2.length ){
+        let itemPart1 = part1[actualPosiPart1]
+        let itemPart2 = part2[actualPosiPart2]
+
+        if (itemPart1.price < itemPart2.price) {
+            result.push(itemPart1)
+            actualPosiPart1++
+        } else {
+            result.push(itemPart2)
+            actualPosiPart2++
+        }
+    }
+    return result.concat(actualPosiPart1 < part1.length ? part1.slice(actualPosiPart1) : part2.slice(actualPosiPart2))
+}
+
+function mergeSort(array) {
+    if (array.length > 1) {
+        const mid = Math.floor(array.length / 2);
+        const part1 = mergeSort(array.slice(0, mid));
+        const part2 = mergeSort(array.slice(mid, array.length));
+        array = ord(part1, part2)
+    }
+    return array
+}
+
+console.log(mergeSort(extremeArray))
--- Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Variables.js
+++ Curso Web Developer/curso_javascript/Aulas/Orientation_Algorithms/Variables.js
@@ -40,4 +40,115 @@ const edData = [
     }
 ]
 
-module.exports = {edFolha, edData}
+const extremeArray = [
+    {
+        name: 'JavaScript',
+        price: 20
+    },
+    {
+        name: 'Python',
+        price: 15
+    },
+    {
+        name: 'Typescript',
+        price: 30
+    },
+    {
+        name: 'Sass',
+        price: 10
+    },
+    {
+        name: 'PostGree',
+        price: 60
+    },
+    {
+        name: 'MongoDB',
+        price: 32
+    },
+    {
+        name: 'GraphQL',
+        price: 78
+    },
+    {
+        name: 'Java',
+        price: 24
+    },
+    {
+        name: 'Haskell',
+        price: 41
+    },
+    {
+        name: 'Prolog',
+        price: 50
+    },
+    {
+        name: 'C',
+        price: 13
+    },
+    {
+        name: 'C++',
+        price: 17
+    },
+    {
+        name: 'C#',
+        price: 19
+    },
+    {
+        name: 'PHP',
+        price: 25
+    },
+    {
+        name: 'Ruby',
+        price: 30
+    },
+    {
+        name: 'R',
+        price: 35
+    },
+    {
+        name: 'Scala',
+        price: 40
+    },
+    {
+        name: 'Swift',
+        price: 45
+    },
+    {
+        name: 'Kotlin',
+        price: 54   
+    },
+    {
+        name: 'Go',
+        price: 64
+    },
+    {
+        name: 'Rust',
+        price: 74
+    },
+    {
+        name: 'Dart',
+        price: 84
+    },
+    {
+        name: 'Elixir',
+        price: 94
+    },
+    {
+        name: 'Erlang',
+        price: 87
+    },
+    {
+        name: 'Zephyr',
+        price: 198
+    },
+    {
+        name: 'F#',
+        price: 101
+    },
+    {
+        name: 'Clojure',
+        price: 110
+    }
+
+]
+module.exports = {edFolha, edData, extremeArray}
