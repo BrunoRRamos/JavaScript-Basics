78e4955f37d8132d49986e7f5c934473b5314a19
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2022-11-15 10:38:27 -0300

    Criando array de links concluido

--- NodeJs/Aulas/SearchBrokenLinks/index.js
+++ NodeJs/Aulas/SearchBrokenLinks/index.js
@@ -5,15 +5,34 @@ function catchError(error) {
     throw new Error(chalk.red(error.code, "Reading Error"));
 }
 
-//Async and Await.
+//Lê uim arquivo .md e retorna um array com objetos {nomeLink : link}.
 async function catchArchiveAsync(archiveRoute) {
     const encoding = 'utf-8';
     try {
         const text = await fs.promises.readFile(archiveRoute, encoding);
-    console.log(chalk.green(text))
+        const relarionLinks = extractLinks(text);
+        console.log(relarionLinks);
     } catch (error) {
         catchError(error);
     }
 }
+
+//Cria um array com {nomeLink : link}.
+function createLinkObj(arr) {
+    let arrLinks = [];
+    for (let i = 0; i < arr.length; i++) {
+        arrLinks.push({[arr[i][1]]: arr[i][2]})
+    }
+    return arrLinks;
+}
+
+//Cria um array com o texto desejado pelo regex.
+function extractLinks(text) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const catches = [...text.matchAll(regex)];
+    const links = createLinkObj(catches)
+    return links;
+}
+
 const archiveRoute = './text.md';
 catchArchiveAsync(archiveRoute)
